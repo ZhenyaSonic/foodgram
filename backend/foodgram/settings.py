1965efcca55a7563d90312d717602bd5903eee84
Евгений Братанов <sonik.zhenyaa@gmail.com>
2024-10-21 23:04:51 +0300

    No_CSRF3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -142,7 +142,7 @@ AUTH_USER_MODEL = 'users.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
