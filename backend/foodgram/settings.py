f605afe45a31a33e517548342230b2030cd4aeec
Евгений Братанов <sonik.zhenyaa@gmail.com>
2024-10-21 22:25:59 +0300

    No_CSRF

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'users.User'
 
-CSRF_TRUSTED_ORIGINS = ['https://evgeniyfoodgram.zapto.org']
+# CSRF_TRUSTED_ORIGINS = ['https://evgeniyfoodgram.zapto.org']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
